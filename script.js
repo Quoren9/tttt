4a7b190899edabc4885791a8998925a82a6bde2d
mensahcalvin <144283801+mensahcalvin@users.noreply.github.com>
2024-06-24 21:41:03 +0000

    updated  the reponsine

--- script.js
+++ script.js
@@ -20,11 +20,11 @@ function showContent(contentId) {
   event.currentTarget.classList.add("active");
 }
 
-function toggleNavbar() {
-  var x = document.getElementById("myNavbar");
-  if (x.className === "navbar") {
-      x.className += " responsive";
-  } else {
-      x.className = "navbar";
-  }
-}
+// function toggleNavbar() {
+//   var x = document.getElementById("myNavbar");
+//   if (x.className === "navbar") {
+//       x.className += " responsive";
+//   } else {
+//       x.className = "navbar";
+//   }
+// }
